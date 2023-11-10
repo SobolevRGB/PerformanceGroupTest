7a17e691a7c6deb9d2cbb91f9737b2769b108f37
SobolevRGB <karos803@gmail.com>
2023-11-10 17:53:49 +0300

    Изменения гит юзера

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s',
     title: 'Тестовое задание',
@@ -19,15 +18,12 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
